5777a4b382f2bcc32714496857e3a9d813ebd14b
Kristian Correa <62035025+KristianCorrea@users.noreply.github.com>
2023-02-26 19:30:39 +0000

    Takes input and returns input length.

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
     System.out.println("Enter a word:");
-    
+    String input = scan.nextLine();
     //Your code will return the length of the entered String
+    System.out.println(input.length());
   }
 }
